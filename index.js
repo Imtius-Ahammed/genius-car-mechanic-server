387b2be4796176bd861d1b8e435167ebe48b3b83
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-04 22:24:12 +0600

    verify token

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req,res,next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unauthorized access'});
+
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, function(err,decoded){
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+  
+}
+
 async function run(){
   try{
     const serviceCollection = client.db('geniusCar').collection('services');
@@ -45,7 +63,12 @@ async function run(){
 
 
 // orders api
-    app.get('/orders',async(req,res)=>{
+    app.get('/orders',verifyJWT, async (req,res)=>{
+     const decoded = req.decoded;
+     console.log('inside orders api', decoded);
+     if(decoded.email !== req.query.email){
+      res.status(403).send({message: 'Forbidden access'})
+     }
       let query = {}
       if(req.query.email){
         query ={
@@ -61,13 +84,13 @@ async function run(){
 
 
     //orders
-    app.post('/orders',async(req,res)=>{
+    app.post('/orders',verifyJWT,async(req,res)=>{
       const order = req.body;
       const result =await orderCollection.insertOne(order);
       res.send(result);
     })
 
-    app.patch('/orders/:id',async(req,res)=>{
+    app.patch('/orders/:id',verifyJWT,async(req,res)=>{
       const id = req.params.id;
       const status = req.body.status;
       const query = {_id : ObjectId(id)};
@@ -84,7 +107,7 @@ async function run(){
     
 // delete
 
-app.delete('/orders/:id',async(req,res)=>{
+app.delete('/orders/:id',verifyJWT,async(req,res)=>{
   const id = req.params.id;
   const query = {_id : ObjectId(id)};
   const result = await orderCollection.deleteOne(query);
